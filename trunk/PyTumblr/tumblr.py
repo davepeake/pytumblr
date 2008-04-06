ed83f668cb60c81a29e5199c37138620974385a4
dave.peake <dave.peake@9741c0a4-ae49-0410-9ba7-5da7213328fb>
2008-04-06 07:33:18 +0000

    First upload of tumblr.py

---
+++ trunk/PyTumblr/tumblr.py
@@ -0,0 +1,71 @@
+import urllib
+
+class CTumblr:
+    init = 0
+    
+    def __init__(self, setemail, setpassword):
+        self.email = setemail
+        self.password = setpassword        
+        self.init = 1
+
+    def send(self,data):
+        if self.init != 1:
+            print "Send Buggered"
+            return 1
+
+        a= urllib.urlopen("http://www.tumblr.com/api/write", data)
+        print a.readlines()
+        return 0
+
+    def quote(self,quote):
+        if self.init != 1:
+            print "Quote Buggered"
+            return 1
+
+        data = urllib.urlencode({"email" : self.email, \
+                                 "password" : self.password, \
+                                 "type" :  "quote", \
+                                 "quote" : quote } )
+
+        self.send(data)
+        return 0
+
+    def photourl(self, url, clickthru=''):
+        if self.init != 1:
+            print "Quote Buggered"
+            return 1
+
+        data = urllib.urlencode({"email" : self.email, \
+                                 "password" : self.password, \
+                                 "type" : "photo", \
+                                 "source" : url, \
+                                 "click-through-url" : clickthru } )
+        self.send(data)
+        return 0
+
+    def videourl(self, url):
+        if self.init != 1:
+            print "Video URL Buggered"
+            return 1
+
+        data = urllib.urlencode({"email" : self.email, \
+                                 "password" : self.password, \
+                                 "type" : "video", \
+                                 "embed" : url } )
+
+        self.send(data)
+        return 0
+
+    def text(self, body, title):
+        if self.init != 1:
+            print "Text Stuffed"
+            return 1
+
+        data = urllib.urlencode({"email" : self.email, \
+                                 "password" : self.password, \
+                                 "type" : "regular", \
+                                 "body" : body , \
+                                 "title" : title } )
+
+        self.send(data)
+        return 0
