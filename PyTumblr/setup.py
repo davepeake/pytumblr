60db900b600a9f858d8494634ee79c96c1ce10a1
dave.peake <dave.peake@9741c0a4-ae49-0410-9ba7-5da7213328fb>
2008-08-16 00:46:46 +0000

    Added a basic setup.py in order to create installer files.

---
+++ PyTumblr/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='pytumblr',
+      version='0.1a',
+      description='A module which provides write access to tumblr using a simple class system.',
+      author='David Peake',
+      author_email='dave.peake@gmail.com',
+      url='http://code.google.com/p/pytumblr/',
+      py_modules=['tumblr']
+      )
